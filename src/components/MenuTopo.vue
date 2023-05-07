7873c680802a430fee6477629fd66b12eb66e2f9
Roberto Junior <burgostecnologia@gmail.com>
2023-05-07 17:29:35 -0300

    primeiro commit

---
+++ src/components/MenuTopo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <a @click="acessarApresentacao()">Home</a>
+      <a @click="acessarConhecimentos()">Conhecimentos Técnicos</a>
+      <a @click="acessarExperiencia()">Experiência</a>
+      <a @click="acessarContato()">Contato</a>
+      <a @click="acessarSobre()">Sobre</a>      
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
