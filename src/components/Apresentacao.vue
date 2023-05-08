cfd27040ffbe800bbfda968d0f8a9397b759a03e
Roberto Burgos <burgostecnologia@gmail.com>
2023-05-07 21:46:22 -0300

    menu e imagem apresentacao

--- src/components/Apresentacao.vue
+++ src/components/Apresentacao.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>apresentacao</h1>
+  <div class="conteudo">
+    <div class="esquerda">
+        <p>Olá, Eu sou o</p>
+        <p>Roberto Burgos</p>
+        <p>Consultor Full-Stack</p>
+        <p>Há mais de 20 anos trabalhando tando em ambientes de grande porte como plataforma Alta</p>
+    </div>
+    <div class="direita">     
+      <div class="forma"></div>
+      <img src="../assets/hero-image6.png" alt="minhaFoto">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +20,33 @@ export default {
 </script>
 
 <style>
+.conteudo{
+  background: white;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  
+}
 
+.direita{
+ 
+  
+  
+}
+.forma{
+  background: #4668f1;
+  width: 600px;
+  height: 450px;
+  border-radius: 500px 300px 0px 100px;
+  
+}
+img{
+  width: 550px;
+  height: 550px;
+  position: relative;
+  top: -551px;
+  right: -50px;
+  
+}
 </style>
--- src/components/MenuTopo.vue
+++ src/components/MenuTopo.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <nav>
+    <nav class="navegacao">
+      <img class="logo" src="../assets/logop.png" alt="logo">      
       <a @click="acessarApresentacao()">Home</a>
       <a @click="acessarConhecimentos()">Conhecimentos Técnicos</a>
       <a @click="acessarExperiencia()">Experiência</a>
       <a @click="acessarContato()">Contato</a>
-      <a @click="acessarSobre()">Sobre</a>      
+      <a @click="acessarSobre()">Sobre</a>   
+      <button>Linkedin</button>   
     </nav>
   </div>
 </template>
@@ -16,6 +18,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+.logo{
+  width: 48px;
+  height: 48px;
+
+}
+.navegacao{
+   background: white;
+   padding: 36px ;
+   color: rgb(159, 162, 163);
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 48px   
+}
+
+button{
+  color: white;
+  background: #4a6cf7;
+  border-radius: 50px;
+}
+
+a {
+  font-weight: 500;
+  color: #494f5b;
+  text-decoration: inherit;
+}
+a:hover {
+  color: #535bf2;
+}
 </style>
